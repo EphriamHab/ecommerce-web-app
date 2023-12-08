8374fd7bfabf5957e1bdac15fabd67e68ab0a694
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-08 23:38:40 +0300

    some package added and modify server.js

--- server.js
+++ server.js
@@ -1,14 +1,32 @@
 import express from 'express';
 import colors from 'colors';
+import dotenv from 'dotenv';
+import morgan from 'morgan';
+import connectDB from './config/db.js';
+import authRoutes from './routes/authRoute.js'
+
+//configure env
+dotenv.config()
+
+// datbase config
+connectDB();
 
 const app = express();
 
+//middlewares
+app.use(express.json())
+app.use(morgan('dev'))
+
+//routes
+app.use('/api/v1/auth', authRoutes)
 app.get('/',(req,res)=>{
 res.send("<h1>Welcome to Ecommerce Full sack project</h1>")
 });
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT,()=>{
-    console.log(`server running on ${PORT}`.bgCyan.white)
+    console.log(
+        `Server Running on ${process.env.DEV_MODE} mode on port ${PORT}`.bgCyan.white
+        );
 })
