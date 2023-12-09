43a6960dab70684e7cc22bb5bea0d7fa5405beaf
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-09 21:36:00 +0300

    Fronted started

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+
+import './App.css';
+import Layout  from './components/Layout/Layout';
+
+function App() {
+  return (
+    <div>
+      <h1>Ecommerce App</h1>
+      <Layout/>
+      
+      
+    </div>
+  );
+}
+
+export default App;
