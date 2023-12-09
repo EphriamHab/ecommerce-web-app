a24d279e5575be0ad83777f4897381620888f673
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-09 21:37:26 +0300

    Login contollers added

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,6 @@
 import userModel from "../models/userModel.js";
-import { hashPassword } from "../helpers/authHelper.js";
+import { comparePassword, hashPassword } from "../helpers/authHelper.js";
+import JWT from "jsonwebtoken"
 export const registerController = async(req,res) =>{
 try {
    const {name,email,password,phone,address} = req.body;
@@ -51,4 +52,70 @@ try {
     })
 }
 };
+
+//post login
+
+export const loginController = async(req,res)=>{
+ try {
+    const {email,password} = req.body;
+
+    //validation
+    if(!email || !password){
+        return res.status(400).send({
+           success:false,
+           message:"Invalid email or Password"
+        });
+    }
+    // check user
+    const user = await userModel.findOne({email})
+    if(!user){
+        return res.status(404).send({
+            success:false,
+            message:"Email is not registerd"
+        })
+    }
+    const match = await comparePassword(password,user.password)
+    if(!match){
+        return res.status(200).send({
+            success:false,
+            message:"Invalid password"
+        })
+    }
+    //token
+    const token = await JWT.sign({_id:user._id},process.env.JWT_SECRET,{
+        expiresIn:'25d',
+    });
+    res.status(200).send({
+        success:true,
+        message:"login successfully",
+        user:{
+           name:user.name,
+           email:user.email,
+           phone:user.phone,
+           address:user.address 
+        },
+        token,
+    })
+
+ } catch (error) {
+    console.log(error);
+    res.status(500).send({
+        success:false,
+        message:"Error in login",
+        error
+    })
+ }
+};
+
+// test controller
+
+export const testController =(req,res)=>{
+    try {
+        res.send("protected Routes"); 
+    } catch (error) {
+        console.log(error);
+        res.send({error});
+    }
+};
+
 
