90d161a3a677087a8c3274430c19af230617a9bc
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-09 21:37:57 +0300

    Login route created

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,10 +1,21 @@
 import express from 'express';
-import {registerController} from '../controllers/authController.js'
+import {
+    registerController,
+    loginController,
+    testController} from '../controllers/authController.js'
+import { requireSignIn,isAdmin } from '../middlewares/authMiddleware.js';
 
 // router object
 
 const router = express.Router();
 
 //routing
+// Register || Method post
 router.post('/register',registerController)
+
+// login || Post
+router.post('/login', loginController)
+
+// test routes
+router.get('/test',requireSignIn,isAdmin,testController)
 export default router
