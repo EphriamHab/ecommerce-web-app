0995afb6a44f1d24ace7fde316f74188b4361255
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 01:02:10 +0300

    Dashboard created

---
+++ client/src/pages/user/Dashboard.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../../components/Layout/Layout'
+
+const Dashboard = () => {
+  console.log("Dashboard is rendered");
+  return (
+    <Layout title={"Dashboard -Ecommerce App"}>
+    <h1>Dashboard page</h1>
+    </Layout>
+  )
+}
+
+export default Dashboard
