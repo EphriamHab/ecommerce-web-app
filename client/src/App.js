b9ff374f12272281ecc6f5e572f8fa35ee2341fb
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 01:07:01 +0300

    APP.js and index.js file modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,7 @@
 
 import './App.css';
+import {ToastContainer} from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import {Routes,Route} from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import About from './pages/About';
@@ -7,14 +9,28 @@ import Contact from './pages/Contact';
 import Pagenotfound from './pages/Pagenotfound';
 import Policy from './pages/Policy';
 import Register from './pages/Auth/Register';
-import {ToastContainer} from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
+import Login from './pages/Auth/Login';
+import Dashboard from './pages/user/Dashboard';
+import PrivateRoute from './components/Routes/Private';
+import ForgotPassword from './pages/Auth/ForgotPassword';
+import AdminDashboard from './pages/Admin/AdminDashboard';
+import AdminRoute from "./components/Routes/AdminRoute";
 function App() {
   return (
     <>
+    <ToastContainer position="top-center"/>
     <Routes>
       <Route path='/' element={<HomePage/>}/>
+      <Route path="/dashboard" element={<PrivateRoute />}>
+          <Route index element={<Dashboard />} />
+          <Route path="user" element={<Dashboard />} />
+      </Route>
+      <Route path="/dashboard" element={<AdminRoute/>}>
+          <Route path="admin" element={<AdminDashboard />} />
+      </Route>
       <Route path='/register' element={<Register/>}/>
+      <Route path='/forgot-password' element={<ForgotPassword/>}/>
+      <Route path='/login' element={<Login/>}/>
       <Route path='/about' element={<About/>}/>
       <Route path='/contact' element={<Contact/>}/>
       <Route path='/policy' element={<Policy/>}/>
--- client/src/index.js
+++ client/src/index.js
@@ -4,14 +4,16 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {BrowserRouter} from 'react-router-dom';
+import { Authprovider } from './context/Auth';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-    <App />
-  </React.StrictMode>
-  </BrowserRouter>
+  <Authprovider>
+     <BrowserRouter>
+       <App />
+     </BrowserRouter>
+  </Authprovider>
+  
 
 );
 
