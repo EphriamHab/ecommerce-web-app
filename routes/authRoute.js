838a7a865df147c4c1435a9da67ecdf678cc521a
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 01:10:10 +0300

    AuthRoute modified

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,7 +2,8 @@ import express from 'express';
 import {
     registerController,
     loginController,
-    testController} from '../controllers/authController.js'
+    testController,
+    forgotPasswordController} from '../controllers/authController.js'
 import { requireSignIn,isAdmin } from '../middlewares/authMiddleware.js';
 
 // router object
@@ -16,6 +17,20 @@ router.post('/register',registerController)
 // login || Post
 router.post('/login', loginController)
 
+//Forgot Password ||Post
+router.post('/forgot-password', forgotPasswordController)
+
+
 // test routes
 router.get('/test',requireSignIn,isAdmin,testController)
+
+//protected User route auth
+router.get('/user-auth', requireSignIn, (req,res) =>{
+    res.status(200).send({ok:true}); 
+})
+
+//protected Admin route auth
+router.get('/admin-auth', requireSignIn, isAdmin, (req,res) =>{
+    res.status(200).send({ok:true}); 
+})
 export default router
