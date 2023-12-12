c4ff2178a3945be78d3ce8dc84b291d1e01c7147
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 17:31:07 +0300

    user Menu and routes for dashboard

--- client/src/components/Layout/AdminMenu.js
+++ client/src/components/Layout/AdminMenu.js
@@ -1,8 +1,33 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 
 const AdminMenu = () => {
   return (
-    <div>AdminMenu</div>
+    <>
+      <div className="text-center">
+        <div className="list-group dashboard-menu">
+          <h4>Admin Panel</h4>
+          <NavLink
+            to="/dashboard/admin/create-category"
+            className="list-group-item list-group-item-action"
+          >
+            Create Category
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/create-product"
+            className="list-group-item list-group-item-action"
+          >
+            Create Product
+          </NavLink>
+          <NavLink
+            to="/dashboard/admin/users"
+            className="list-group-item list-group-item-action"
+          >
+            Users
+          </NavLink>
+        </div>
+      </div>
+    </>
   )
 }
 
