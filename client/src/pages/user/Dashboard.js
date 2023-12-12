4a4f168051715f67c8ffc9b8df079dabc6454812
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 17:32:46 +0300

    Admin dashboard with there components

--- client/src/pages/user/Dashboard.js
+++ client/src/pages/user/Dashboard.js
@@ -1,11 +1,26 @@
 import React from 'react'
 import Layout from '../../components/Layout/Layout'
-
+import UserMenu from '../../components/Layout/UserMenu';
+import { useAuth } from '../../context/Auth';
 const Dashboard = () => {
+  const [auth] = useAuth();
   console.log("Dashboard is rendered");
   return (
     <Layout title={"Dashboard -Ecommerce App"}>
-    <h1>Dashboard page</h1>
+       <div className='container-fluid m-3 p-3'>
+       <div className='row'>
+       <div className='col-md-3'>
+          <UserMenu/>
+       </div>
+       <div className='col-md-9'>
+          <div className='card w-75 p-3'>
+              <h3>{auth?.user?.name}</h3>
+              <h3>{auth?.user?.email}</h3>
+              <h3>{auth?.user?.address}</h3>
+          </div>
+       </div>
+       </div>
+       </div>
     </Layout>
   )
 }
