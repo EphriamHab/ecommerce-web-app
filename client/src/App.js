35bbaf726427236616da65cf5f8afe06d1ed74a8
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-12 17:33:46 +0300

    App.js is modified

--- client/src/App.js
+++ client/src/App.js
@@ -15,6 +15,11 @@ import PrivateRoute from './components/Routes/Private';
 import ForgotPassword from './pages/Auth/ForgotPassword';
 import AdminDashboard from './pages/Admin/AdminDashboard';
 import AdminRoute from "./components/Routes/AdminRoute";
+import CreateCategory from './pages/Admin/CreateCategory';
+import CreateProduct from './pages/Admin/CreateProduct';
+import Users from './pages/Admin/Users';
+import Orders from './pages/user/Orders';
+import Profile from './pages/user/Profile';
 function App() {
   return (
     <>
@@ -24,9 +29,14 @@ function App() {
       <Route path="/dashboard" element={<PrivateRoute />}>
           <Route index element={<Dashboard />} />
           <Route path="user" element={<Dashboard />} />
+          <Route path="user/orders" element={<Orders />} />
+          <Route path="user/profile" element={<Profile/>} />
       </Route>
       <Route path="/dashboard" element={<AdminRoute/>}>
-          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin" element={<AdminDashboard />}/>
+          <Route path="admin/create-category" element={<CreateCategory/>}/>
+          <Route path="admin/create-product" element={<CreateProduct/>}/>
+          <Route path="admin/users" element={<Users/>}/>
       </Route>
       <Route path='/register' element={<Register/>}/>
       <Route path='/forgot-password' element={<ForgotPassword/>}/>
