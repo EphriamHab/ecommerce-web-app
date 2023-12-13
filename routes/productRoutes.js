a3297f3fca526f02dfb8de1765507c3d5369d5b8
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-14 00:07:41 +0300

    Product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,46 @@
 import express from 'express'
 import { isAdmin, requireSignIn } from '../middlewares/authMiddleware.js';
-import { createProductController } from '../controllers/productController.js';
+import {
+    createProductController,
+    getProductController,
+    getSingleProductController,
+    productPhotoController,
+    deleteProductController,
+    updateProductController
+} from '../controllers/productController.js';
+import formidable from 'express-formidable';
 
 const router = express.Router();
 
-//routes
+// create product routes
 router.post(
     '/create-product',
     requireSignIn,
     isAdmin,
+    formidable(),
     createProductController)
+
+// update product
+router.put(
+    '/update-product/:pid',
+    requireSignIn,
+    isAdmin,
+    formidable(),
+    updateProductController)
+
+// get products
+router.get('/get-product', getProductController)
+
+//single product
+router.get('/get-product/:slug', getSingleProductController)
+
+//get photo
+router.get('/product-photo/:pid', productPhotoController)
+
+//delete product
+router.delete('/product/:pid',
+    requireSignIn,
+    isAdmin,
+    deleteProductController)
 
 export default router
