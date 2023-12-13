6932d5df907c07de94196a1e86002a105c919231
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-13 23:58:55 +0300

    Product Admin menu added

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom'
 const Footer = () => {
   return (
     <div className='footer'>
-        <h3 className='text-center'>All Right Reserved &copy; Ephrem</h3>
-        <p className='text-center mt-3'>
-          <Link to="/about">About</Link>|
-          <Link to="/contact">Contact</Link>|
-          <Link to="/policy">Privacy Policy</Link>
-        </p>
+      <h3 className='text-center'>All Right Reserved &copy; Ephrem</h3>
+      <p className='text-center mt-3'>
+        <Link to="/about">About</Link>|
+        <Link to="/contact">Contact</Link>|
+        <Link to="/policy">Privacy Policy</Link>
+      </p>
     </div>
   )
 }
