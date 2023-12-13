aa3769e62d0454d59bebef743cc9937b945cc537
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-14 00:02:33 +0300

    Some Route are added

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,8 @@
 
 import './App.css';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
-import {Routes,Route} from 'react-router-dom'
+import { Routes, Route } from 'react-router-dom'
 import HomePage from './pages/HomePage';
 import About from './pages/About';
 import Contact from './pages/Contact';
@@ -20,32 +20,34 @@ import CreateProduct from './pages/Admin/CreateProduct';
 import Users from './pages/Admin/Users';
 import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
+import Products from './pages/Admin/Products';
 function App() {
   return (
     <>
-    <ToastContainer position="top-center"/>
-    <Routes>
-      <Route path='/' element={<HomePage/>}/>
-      <Route path="/dashboard" element={<PrivateRoute />}>
+      <ToastContainer position="top-center" />
+      <Routes>
+        <Route path='/' element={<HomePage />} />
+        <Route path="/dashboard" element={<PrivateRoute />}>
           <Route index element={<Dashboard />} />
           <Route path="user" element={<Dashboard />} />
           <Route path="user/orders" element={<Orders />} />
-          <Route path="user/profile" element={<Profile/>} />
-      </Route>
-      <Route path="/dashboard" element={<AdminRoute/>}>
-          <Route path="admin" element={<AdminDashboard />}/>
-          <Route path="admin/create-category" element={<CreateCategory/>}/>
-          <Route path="admin/create-product" element={<CreateProduct/>}/>
-          <Route path="admin/users" element={<Users/>}/>
-      </Route>
-      <Route path='/register' element={<Register/>}/>
-      <Route path='/forgot-password' element={<ForgotPassword/>}/>
-      <Route path='/login' element={<Login/>}/>
-      <Route path='/about' element={<About/>}/>
-      <Route path='/contact' element={<Contact/>}/>
-      <Route path='/policy' element={<Policy/>}/>
-      <Route path='/pagenotfound' element={<Pagenotfound/>}/>
-    </Routes>
+          <Route path="user/profile" element={<Profile />} />
+        </Route>
+        <Route path="/dashboard" element={<AdminRoute />}>
+          <Route path="admin" element={<AdminDashboard />} />
+          <Route path="admin/create-category" element={<CreateCategory />} />
+          <Route path="admin/create-product" element={<CreateProduct />} />
+          <Route path="admin/products" element={<Products />} />
+          <Route path="admin/users" element={<Users />} />
+        </Route>
+        <Route path='/register' element={<Register />} />
+        <Route path='/forgot-password' element={<ForgotPassword />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/policy' element={<Policy />} />
+        <Route path='*' element={<Pagenotfound />} />
+      </Routes>
     </>
   );
 }
