129be76eacb49cdb6436440c348d73951d4f4018
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-15 23:12:39 +0300

    Some route are added

--- client/src/App.js
+++ client/src/App.js
@@ -22,12 +22,18 @@ import Orders from './pages/user/Orders';
 import Profile from './pages/user/Profile';
 import Products from './pages/Admin/Products';
 import UpdateProduct from './pages/Admin/UpdateProduct';
+import Search from './pages/Search';
+import ProductDetails from './pages/ProductDetails';
+import Categories from './pages/Categories';
 function App() {
   return (
     <>
       <ToastContainer position="top-center" />
       <Routes>
         <Route path='/' element={<HomePage />} />
+        <Route path='/product/:slug' element={<ProductDetails/>} />
+        <Route path='/categories' element={<Categories/>} />
+        <Route path='/search' element={<Search/>} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route index element={<Dashboard />} />
           <Route path="user" element={<Dashboard />} />
