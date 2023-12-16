472f083ab3da99a655c20bc299955dced3cfb474
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-16 22:26:08 +0300

    Some routes are added

--- client/src/App.js
+++ client/src/App.js
@@ -25,6 +25,8 @@ import UpdateProduct from './pages/Admin/UpdateProduct';
 import Search from './pages/Search';
 import ProductDetails from './pages/ProductDetails';
 import Categories from './pages/Categories';
+import CategoryProduct from './pages/CategoryProduct';
+import CartPage from './pages/CartPage';
 function App() {
   return (
     <>
@@ -33,6 +35,8 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='/product/:slug' element={<ProductDetails/>} />
         <Route path='/categories' element={<Categories/>} />
+        <Route path='/cart' element={<CartPage/>} />
+        <Route path='/category/:slug' element={<CategoryProduct/>} />
         <Route path='/search' element={<Search/>} />
         <Route path="/dashboard" element={<PrivateRoute />}>
           <Route index element={<Dashboard />} />
