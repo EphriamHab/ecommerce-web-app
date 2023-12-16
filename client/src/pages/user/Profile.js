25806dd9f940c66aa0389419ff79b96e7e580cb7
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-16 22:28:47 +0300

    Profile page modified

--- client/src/pages/user/Profile.js
+++ client/src/pages/user/Profile.js
@@ -1,22 +1,130 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
-import UserMenu from '../../components/Layout/UserMenu'
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout/Layout";
+import UserMenu from "../../components/Layout/UserMenu";
+import { useAuth } from "../../context/Auth";
+import { toast } from "react-toastify";
+import axios from "axios";
 
 const Profile = () => {
-  return (
-    <Layout title={'Your profile'}>
-       <div className='container-fluid m-3 p-3'>
-          <div className='row'>
-              <div className='col-md-3'>
-                 <UserMenu/>
-              </div>
-              <div className='col-md-9'>
-                <h1>Your profile</h1>
-              </div>
-           </div>
-      </div> 
-    </Layout>
-  )
-}
+  //context
+  const [auth, setAuth] = useAuth();
+  //state
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [phone, setPhone] = useState(auth?.user?.phone || "");
+  const [address, setAddress] = useState("");
 
-export default Profile
+
+  //get user data
+  useEffect(()=>{
+    const {email,name,address} = auth?.user
+    setName(name)
+    setEmail(email)
+    setPhone(phone)
+    setAddress(address)
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  },[auth?.user])
+  // form function
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const { data } = await axios.put("/api/v1/auth/profile", {
+        name,
+        email,
+        password,
+        phone,
+        address,
+      });
+      if (data?.error) {
+        toast.error(data?.error);
+      } else {
+        setAuth({ ...auth, user: data?.updatedUser });
+        let ls = localStorage.getItem("auth");
+        ls = JSON.parse(ls);
+        ls.user = data.updatedUser;
+        localStorage.setItem("auth", JSON.stringify(ls));
+        toast.success("Profile Updated Successfully");
+      }
+    } catch (error) {
+      console.log(error);
+      toast.error("Something went wrong");
+    }
+  };
+  return (
+    <Layout title={"Your profile"}>
+      <div className="container-fluid m-3 p-3 dashboard">
+        <div className="row">
+          <div className="col-md-3">
+            <UserMenu />
+          </div>
+          <div className="col-md-8">
+            <div className="form-container" style={{ marginTop: "-40px" }}>
+              <form onSubmit={handleSubmit}>
+                <h4 className="title">USER PROFILE</h4>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={name}
+                    onChange={(e) => setName(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Name"
+                    autoFocus
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Email "
+                    disabled
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    className="form-control"
+                    id="exampleInputPassword1"
+                    placeholder="Enter Your Password"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={phone}
+                    onChange={(e) => setPhone(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Phone"
+                  />
+                </div>
+                <div className="mb-3">
+                  <input
+                    type="text"
+                    value={address}
+                    onChange={(e) => setAddress(e.target.value)}
+                    className="form-control"
+                    id="exampleInputEmail1"
+                    placeholder="Enter Your Address"
+                  />
+                </div>
+
+                <button type="submit" className="btn btn-primary">
+                  UPDATE
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Profile;
