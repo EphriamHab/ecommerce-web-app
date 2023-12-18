9025918919dace8653874f80801bbdb50b6f1f66
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-18 23:22:04 +0300

    Price Array modified

--- client/src/components/Prices.js
+++ client/src/components/Prices.js
@@ -2,33 +2,27 @@
 export const Prices = [
     {
         _id: 0,
-        name: "$0 to 19",
-        array: [0, 19]
+        name: "$0 to 999",
+        array: [0, 999]
     },
     {
         _id: 1,
-        name: "$20 to 39",
-        array: [20, 39]
+        name: "$1000 to 1499",
+        array: [1000, 1499]
     },
     {
         _id: 2,
-        name: "$40 to 59",
-        array: [0, 19]
+        name: "$1500 to 1999",
+        array: [1500, 1999]
     },
     {
         _id: 3,
-        name: "$60 to 79",
-        array: [60, 79]
+        name: "$2000 to 2499",
+        array: [2000, 2499]
     },
     {
         _id: 4,
-        name: "$80 to 99",
-        array: [80, 99]
+        name: "$2500 0r more",
+        array: [2500, 9999]
     },
-    {
-        _id: 5,
-        name: "$100 or more",
-        array: [100, 9999]
-    },
-
 ]
