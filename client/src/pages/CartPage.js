e94f63e061967f26215ec859904e25582115ffb8
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-19 01:13:01 +0300

    Add styling css

--- client/src/pages/CartPage.js
+++ client/src/pages/CartPage.js
@@ -6,6 +6,8 @@ import { useNavigate } from "react-router-dom";
 import DropIn from "braintree-web-drop-in-react";
 import axios from "axios";
 import { toast } from "react-toastify";
+import "../styles/CartStyles.css";
+
 const CartPage = () => {
   const [auth, setAuth] = useAuth();
   const [cart, setCart] = useCart();
