0f1a404e2459a5b44d07e92fe6dce6f1edc96886
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-19 01:14:34 +0300

    Some controller added

--- controllers/authController.js
+++ controllers/authController.js
@@ -231,7 +231,7 @@ export const getAllOrdersController = async(req,res)=>{
     .find({})
     .populate("products","-photo")
     .populate("buyer","name")
-    .sort({createdAt: "-1"})
+    .sort({createdAt: -1})
     res.json(orders)
    } catch (error) {
     console.log(error)
@@ -241,4 +241,21 @@ export const getAllOrdersController = async(req,res)=>{
       error
     })
    }
+}
+
+//order status update
+export const orderStatusController = async(req,res) =>{
+      try {
+        const {orderId} = req.params;
+        const {status} = req.body;
+        const orders = await orderModel.findByIdAndUpdate(orderId, {status},{new:true})
+        res.json(orders)
+      } catch (error) {
+        console.log(error)
+        res.status(500).send({
+          success:false,
+          message:"Error in updting Status",
+          error
+        })
+      }
 }
