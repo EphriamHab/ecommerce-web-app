3b16dcbc2362c416950434765b83869b7c8f5e1b
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-19 22:51:57 +0300

    Client build for deploy

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import authRoutes from './routes/authRoute.js'
 import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
+import path from 'path'
 
 //configure env
 dotenv.config()
@@ -20,7 +21,12 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
+app.use(express.static(path.join(__dirname, './client/build')))
 
+//rest api
+app.use('*', function(req,res){
+   res.sendFile(path.join(__dirname, './client/build/index.html'));
+})
 //routes
 app.use('/api/v1/auth', authRoutes)
 app.use('/api/v1/category', categoryRoutes)
