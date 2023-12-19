0b30046a11fef9e1df4664062f89228444e2587b
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-19 23:46:07 +0300

    es module fix

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
+import { fileURLToPath } from 'url';
 
 //configure env
 dotenv.config()
@@ -15,6 +16,9 @@ dotenv.config()
 // datbase config
 connectDB();
 
+//es module fix
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 const app = express();
 
 //middlewares
