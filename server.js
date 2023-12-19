9ea83d260d19a1f7aade01cae11c523823a0960c
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-20 00:20:35 +0300

    modified path

--- server.js
+++ server.js
@@ -25,11 +25,11 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, './client/build')))
+app.use(express.static(path.join(__dirname, '/var/task/client/build')))
 
 //rest api
 app.use('*', function(req,res){
-   res.sendFile(path.join(__dirname, './client/build/index.html'));
+   res.sendFile(path.join(__dirname, '/var/task/client/build/index.html'));
 })
 //routes
 app.use('/api/v1/auth', authRoutes)
