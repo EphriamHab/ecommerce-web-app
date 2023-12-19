26370aa35f9ccfed782e4115e50bbff270299bdb
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-20 00:49:28 +0300

    Server modified

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ import categoryRoutes from './routes/categoryRoutes.js'
 import productRoutes from './routes/productRoutes.js'
 import cors from 'cors'
 import path from 'path'
-import { fileURLToPath } from 'url';
+
 
 //configure env
 dotenv.config()
@@ -16,24 +16,14 @@ dotenv.config()
 // datbase config
 connectDB();
 
-//es module fix
-const __filename = fileURLToPath(import.meta.url)
-const __dirname = path.dirname(__filename)
+
 const app = express();
 
-const cyclicWorkingDir = process.env.CYCLIC_WORKING_DIR || process.cwd();
-const absoluteBuildPath = path.resolve(cyclicWorkingDir, 'client', 'build');
+
 //middlewares
 app.use(cors())
 app.use(express.json())
 app.use(morgan('dev'))
-app.use(express.static(absoluteBuildPath))
-
-//rest api
-app.use('*', function(req,res){
-    const absoluteIndexPath = path.join(absoluteBuildPath, 'index.html');
-    res.sendFile(absoluteIndexPath);
-})
 
 //routes
 app.use('/api/v1/auth', authRoutes)
