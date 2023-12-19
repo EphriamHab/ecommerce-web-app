56db08b283cdb73190352427b44f2142a80fdb1b
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-19 22:01:43 +0300

    Users page created

--- client/src/pages/Admin/Users.js
+++ client/src/pages/Admin/Users.js
@@ -1,22 +1,59 @@
-import React from 'react'
-import Layout from '../../components/Layout/Layout'
-import AdminMenu from '../../components/Layout/AdminMenu'
+import React, { useState, useEffect } from "react";
+import Layout from "../../components/Layout/Layout";
+import AdminMenu from "../../components/Layout/AdminMenu";
+import axios from "axios";
 
 const Users = () => {
-  return (
-    <Layout title={'Dashboard - All users'}>
-        <div className='container-fluid m-3 p-3'>
-        <div className='row'>
-            <div className='col-md-3'>
-                <AdminMenu/>
-            </div>
-            <div className='col-md-3'>
-                 <h1>All Users</h1>
-            </div>
-        </div>
-        </div>
-    </Layout>
-  )
-}
+  const [users, setUsers] = useState([]);
 
-export default Users
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const response = await axios.get("/api/v1/auth/all-users");
+        setUsers(response.data);
+      } catch (error) {
+        console.error("Error fetching users:", error);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+  return (
+    <Layout title={"Dashboard - All users"}>
+      <div className="container-fluid m-3 p-3">
+        <div className="row">
+          <div className="col-md-3">
+            <AdminMenu />
+          </div>
+          <div className="col-md-9">
+            <h1 className="text-center">All Users</h1>
+            <table className="table">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Email</th>
+                  <th>Phone</th>
+                  <th>Address</th>
+                  <th>Role</th>
+                </tr>
+              </thead>
+              <tbody>
+                {users.map((user) => (
+                  <tr key={user._id}>
+                    <td>{user.name}</td>
+                    <td>{user.email}</td>
+                    <td>{user.phone}</td>
+                    <td>{user.address}</td>
+                    <td>{user.role === 0 ? "User" : "Admin"}</td>
+                  </tr>
+                ))}
+              </tbody>
+            </table>
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
+};
+
+export default Users;
