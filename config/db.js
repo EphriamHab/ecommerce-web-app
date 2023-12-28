a4dafc436fea1d061f8221670aede2c14d9f06d7
EphriamHab <ephremhabtamu2015@gmail.com>
2023-12-28 16:15:04 +0300

    DB connection time out increase

--- config/db.js
+++ config/db.js
@@ -3,7 +3,7 @@ import colors from 'colors'
 
 const connectDB = async()=>{
     try {
-       const conn = await mongoose.connect(process.env.MONGODB_URL);
+       const conn = await mongoose.connect(process.env.MONGODB_URL,{connectTimeoutMS: 30000 });
        console.log(`Connected to Mongodb ${conn.connection.host}`.bgMagenta.white);
     } catch (error) {
         console.log(`Error in Mongodb ${error}`.bgRed)
