b2dfbd79654fb451af2ac709a49fb7cc78530440
EphriamHab <ephremhabtamu2015@gmail.com>
2025-04-02 10:06:55 +0300

    server

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 import express from "express";
-import colors from "colors/index.js";
+import colors from "colors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 import connectDB from "./config/db.js";
