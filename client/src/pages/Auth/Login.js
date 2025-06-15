b53d5a6a2559b031b60eda331bd541216f48b7d4
EphriamHab <ephremhabtamu2015@gmail.com>
2025-06-15 23:16:59 +0300

    login

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -13,7 +13,7 @@ const Login = () => {
   const navigate = useNavigate();
   const location = useLocation();
 
-  //form function
+  // Handle Login Submission
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
@@ -21,8 +21,8 @@ const Login = () => {
         `https://ecommerce-web-app-gcjn.vercel.app/api/v1/auth/login`,
         { email, password }
       );
-      if (res && res.data.success) {
-        toast.success(res.data && res.data.message);
+      if (res?.data?.success) {
+        toast.success(res.data.message);
         setAuth({
           ...auth,
           user: res.data.user,
@@ -34,53 +34,52 @@ const Login = () => {
         toast.error(res.data.message);
       }
     } catch (error) {
-      console.log(error);
+      console.error(error);
       toast.error("Something went wrong");
     }
   };
+
   return (
     <Layout title={"Login - Ecommerce App"}>
       <div className="form-container">
-        <h1>Login</h1>
         <form onSubmit={handleSubmit}>
+          <h1>Login</h1>
+
           <div className="mb-3">
             <input
               type="email"
               value={email}
               onChange={(e) => setEmail(e.target.value)}
               className="form-control"
-              id="exampleInputEmail1"
               placeholder="Enter Your Email"
               required
             />
           </div>
+
           <div className="mb-3">
             <input
               type="password"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               className="form-control"
-              id="exampleInputPassword1"
               placeholder="Enter Your Password"
               required
             />
           </div>
+
           <div className="mb-3">
             <button type="submit" className="btn btn-primary">
               LOGIN
             </button>
           </div>
-          <div className="mb-3">
-            <button
-              type="button"
-              className="btn btn-primary"
-              onClick={() => {
-                navigate("/forgot-password");
-              }}
-            >
-              Forgot Password
-            </button>
-          </div>
+
+          <button
+            type="button"
+            className="forgot-btn"
+            onClick={() => navigate("/forgot-password")}
+          >
+            Forgot Password?
+          </button>
         </form>
       </div>
     </Layout>
