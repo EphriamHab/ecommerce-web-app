5e0a7f7fbdedfc7d078dd4899a0aae3ac0fba4d6
EphriamHab <ephremhabtamu2015@gmail.com>
2025-06-16 13:49:37 +0300

    search

--- client/src/components/Form/SearchInput.js
+++ client/src/components/Form/SearchInput.js
@@ -34,7 +34,7 @@ const SearchInput = () => {
           value={values.keyword}
           onChange={(e) => setValues({ ...values, keyword: e.target.value })}
         />
-        <button className="btn btn-outline-success" type="submit">
+        <button className="btn btn-outline-primary" type="submit">
           Search
         </button>
       </form>
